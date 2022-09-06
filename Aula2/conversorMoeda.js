16b7bf75d820bcb7421f92f68f2bcc685a2caab8
Lucas Cristi <lucasccristi@gmail.com>
2022-09-06 11:27:50 -0300

    Realizado projeto para conversão de moeda

---
+++ Aula2/conversorMoeda.js
@@ -0,0 +1,8 @@
+function converter(){
+
+    let valor = document.getElementById("dolar").value
+    let dolarAtual = document.getElementById("dolarAtual").value
+
+    let calculo = valor * dolarAtual
+    document.getElementById("resultado").innerHTML = `Valor em reais R$ ${calculo}`
+}
